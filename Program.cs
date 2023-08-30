26d4f7db3e839dd9888f8535c2960e689ad0ba5a
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-08-30 10:26:06 -0300

    basic Console template

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BlackBox_Proyect_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
