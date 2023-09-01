8d9abddcf886cf218fc2efb511276ffa0ea0e748
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-09-01 13:08:01 -0300

    update  colgado

--- Hung.cs
+++ Hung.cs
@@ -12,33 +12,24 @@ namespace BlackBox_Proyect_One
             // bool running=true;
             // while (running)
             // {
-                Tls.colorFlip(ConsoleColor.Black,ConsoleColor.DarkRed);
-                Console.SetCursorPosition(x, y);
-                Draw.down(7,'█',null,null);
-                Draw.right(5,'█',null,null);
-                Console.SetCursorPosition(x, y);
-                Draw.right(8,'█',null,null);
-                Draw.down(2,'│','█',null);
+            Tls.colorFlip(ConsoleColor.Black,ConsoleColor.DarkRed);
+            Console.SetCursorPosition(x, y);
+            Draw.down(7,'█',null,null);
+            Draw.right(5,'█',null,null);
+            Console.SetCursorPosition(x, y);
+            Draw.right(8,'█',null,null);
+            Draw.down(2,'│','█',null);
                 
             Console.SetCursorPosition(x, y);
 
-                if (wrong==1){rightFeet=true;}
-                if (wrong==2){leftFeet=true;}
-                if (wrong==3){torso=true;}
-                if (wrong==4){righHand=true;}
-                if (wrong==5){leftHand=true;}
-                if (wrong==6){head=true;}//running=false;}
-
-                Tls.colorFlip(ConsoleColor.Black,ConsoleColor.Red);
+            Tls.colorFlip(ConsoleColor.Black,ConsoleColor.Red);
                 
-                if (rightFeet){Draw.point(x+9,y+4,'\\');}
-                if (leftFeet){Draw.point(x+7,y+4,'/');}
-                if (torso){Draw.point(x+8,y+3,'|');}
-                if (righHand){Draw.point(x+9,y+3,'\\');}
-                if (leftHand){Draw.point(x+7,y+3,'/');}
-                if (head){Draw.point(x+8,y+2,'0'); Tls.ParalelOut(x+12,y+5,"You Lost");}
-        //}
-            
+            if (rightFeet){Draw.point(x+9,y+4,'\\');}
+            if (leftFeet){Draw.point(x+7,y+4,'/');}
+            if (torso){Draw.point(x+8,y+3,'|');}
+            if (righHand){Draw.point(x+9,y+3,'\\');}
+            if (leftHand){Draw.point(x+7,y+3,'/');}
+            if (head){Draw.point(x+8,y+2,'0'); Tls.ParalelOut(x+12,y+5,"You Lost");}
         } // end Guy method
 
         public static void GuessingWord()
@@ -67,15 +58,19 @@ namespace BlackBox_Proyect_One
             while(gameGoing)
             {
                 int x=0,y=0;
-                Guy(x,y,rightFeet, leftFeet, torso, righHand, leftHand, head, wrongAttempts);
-
                 Console.Clear();
-                Console.SetCursorPosition(20, 10);
-                Console.WriteLine("guess the word: " + hiddenWord);
-                Console.WriteLine($"Wrong Attemps: {wrongAttempts}");
+                Guy(x,y,rightFeet, leftFeet, torso, righHand, leftHand, head, wrongAttempts);
+                
+                x = 20;y=10;
+                // Console.SetCursorPosition(x,y);
+                Tls.ParalelOut(x,y,hiddenWord);
+                Tls.ParalelOut(x,y+1,$"Wrong Attemps: {wrongAttempts}");
+                // Console.WriteLine("guess the word: " + hiddenWord);
+                // Console.WriteLine($"Wrong Attemps: {wrongAttempts}");
 
                 // Leer la letra ingresada por el jugador
-                Console.Write("Write a Letter: ");
+                // Console.Write("Write a Letter: ");
+                Tls.ParalelOut(x,y+2,"Write a Letter: ");
                 char letra = Console.ReadKey().KeyChar;
 
                 if (secretWord.Contains(letra))
@@ -101,10 +96,17 @@ namespace BlackBox_Proyect_One
                     else
                     {
                         wrongAttempts++;
+                        if (wrongAttempts==1){rightFeet=true;}
+                        if (wrongAttempts==2){leftFeet=true;}
+                        if (wrongAttempts==3){torso=true;}
+                        if (wrongAttempts==4){righHand=true;}
+                        if (wrongAttempts==5){leftHand=true;}
                         if(wrongAttempts == 6)
                         {
-                            //Console.WriteLine("¡YOU LOST!");
-                            Console.WriteLine("The Word Is: " + secretWord);
+                            head=true;
+                            // Console.WriteLine("¡YOU LOST!");
+                            Tls.ParalelOut(x,y,"The Word Is: " + secretWord);
+                            // Console.WriteLine("The Word Is: " + secretWord);
                             gameGoing = false;
                         }
                     }
